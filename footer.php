f294266da2c288f1c3009ea043ef10e7b3d97886
Yash Kochar <yash.kochar143@gmail.com>
2020-11-02 20:55:17 +0530

    Baba ka Dhaba Final2

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
     <div class="col-sm-12 text-center">
       <div class="social-icon">
         <a href="index.php"><i class="fa fa-facebook"></i></a>
-        <a href="index.php"><i class="fa fa-twitter"></i></a>
+        <a href="twitter.com/theyashkochar"><i class="fa fa-twitter"></i></a>
         <a href="index.php"><i class="fa fa-pinterest"></i></a>
         <a href="index.php"><i class="fa fa-rss"></i></a>
       </div>
